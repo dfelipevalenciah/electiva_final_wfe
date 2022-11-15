0d5600169ddfab9c902254be318fd9c4ac46983b
Hector Andres Diaz <pipefrtin78@gmail.com>
2022-11-14 21:38:53 -0500

    seeder para persona completo

--- seeders/20221108133838-demo-persona.js
+++ seeders/20221108133838-demo-persona.js
@@ -3,23 +3,19 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('personas',[{
+      nombre_persona: 'Andres',
+      telefono_persona: '32122251225',
+      apellidos_persona: 'Diaz',
+      email_persona:'andres1@gmail.com',
+      edad_persona:'98',
+      lugar_residencia_persona: 'calle 8 # 11 - 22',
+      token_google:'225443'
+    }]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('personas', null, {});
+
   }
 };
