cc520557433663694752eec652322c84b8f0d027
Hector Andres Diaz <pipefrtin78@gmail.com>
2022-11-15 11:51:28 -0500

    actualizacion persona lleno

--- seeders/20221108133838-demo-persona.js
+++ seeders/20221108133838-demo-persona.js
@@ -3,8 +3,20 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('personas',[{
-    
+      return queryInterface.bulkInsert('personas',[{
+        numero_identificacion: '6588789',
+        nombres_persona: 'Daniel',
+        telefono_persona: '3265445789',
+        apellidos_persona: 'Valencia',
+        email_persona:'caniel1@gmail.com',
+        edad_persona:'23',  
+        lugar_residencia_persona: 'calle 8 # 7 - 22',
+        profesions_id_profesions: 1,
+        servicio_id_servicio: 2,
+        registros_id_registros:1,
+        token_google:'001234',
+        createdAt: new Date(),
+        updatedAt: new Date()
     }]);
   },
 
