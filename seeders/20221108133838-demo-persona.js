4b619bf0d1fe91b25d5d7687a3c8218b95d5b79c
Hector Andres Diaz <pipefrtin78@gmail.com>
2022-11-15 11:31:31 -0500

    correccion seeders

--- seeders/20221108133838-demo-persona.js
+++ seeders/20221108133838-demo-persona.js
@@ -11,6 +11,9 @@ module.exports = {
       email_persona:'caniel1@gmail.com',
       edad_persona:'23',
       lugar_residencia_persona: 'calle 6 # 7 - 22',
+      profesions_id_profesions: 1,
+      servicio_id_servicio: 2,
+      registros_id_registros:1,
       token_google:'001234',
       createdAt: new Date(),
       updatedAt: new Date()
