052ee7ef555e9d825e11b51545b7fee1142ec3ca
Felipe Valencia <dfelipevalenciah21@gmail.com>
2022-11-14 20:48:07 -0500

    controller regitro y profesion

--- controllers/RegistroControllers.js
+++ controllers/RegistroControllers.js
@@ -17,5 +17,43 @@ ListAt(req,res){
               .then(registro => res.status(200).send(registro))
               .catch(error => res.status(400).send(error))
 },
+DeleteRegistro(req, res) {
+     return registro
+       .destroy({
+         where: {
+           id_registro: req.params.id,
+         },
+       })
+       .then((registro) => res.sendStatus(registro))
+       .catch((error) => res.status(400).send(error));
+   },
+   UpdateRegistro(req, res) {
+     return registro
+       .update(
+         {
+         correo: req.body.correo,
+         contrasenia: req.body.contrasenia,
+         nombre_usuario: req.body.nombre_usuario,
+         },
+         {
+           where: {
+             id_registro: req.params.id,
+           },
+         }
+       )
+       .then((result) => {
+         res.json(result);
+       });
+   },
+   CreateRegistro(req, res) {
+     return registro
+       .create({
+       correo: req.params.correo,
+       contrasenia: req.params.contrasenia,
+       nombre_usuario: req.params.nombre_usuario,
+       })
+       .then((registro) => res.status(200).send(registro))
+       .catch((error) => res.status(400).send(error));
+   },
 
 }
